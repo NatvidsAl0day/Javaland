023fa6065260b519e1cd7f6b809abf375a0fe358
Natvids Al0day <168284843+NatvidsAl0day@users.noreply.github.com>
2025-07-09 16:39:30 +0700

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:4000/api/v1';
+// export const BASE_URL = 'http://localhost:4000/api/v1';
+export const BASE_URL = 'https://javaland-api.vercel.app/api/v1';
 // export const BASE_URL = 'https://tr-back.vercel.app/api/v1';
